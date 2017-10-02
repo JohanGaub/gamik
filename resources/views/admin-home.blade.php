bff2f9f8e90381fe5b92a38aec5283755b3c8c89
JohanGaub <johangaub@gmail.com>
2017-10-02 16:37:04 +0200

    Try to compile SCSS to CSS

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -4,31 +4,26 @@
 @endsection
 @section('contenu')
 
-    <div class=container">
 
-        <div class="row d-flex justify-content-around">
-
-            <div class="cardtry">
-
-            <div class="card">
-                <img class="card-img-top" src="../images/games.jpg" alt="games image">
-                <div class="card-body">
-                    <h4 class="card-title">Games</h4>
-                    <p class="card-text">Latest reviewed</p>
-                    <div class="list-group list-group-flush">
-                        <a href="#" class="list-group-item">Cras justo odio</a>
-                        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                        <a href="#" class="list-group-item">Morbi leo risus</a>
-                        <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                        <a href="#" class="list-group-item">Vestibulum at eros</a>
+        <div class="row d-flex justify-content-around ESSAI">
+            <div class="cardtry ESSAI">
+                <div class="card">
+                    <img class="card-img-top" src="../images/games.jpg" alt="games image">
+                    <div class="card-body ESSAI">
+                        <h4 class="card-title">Games</h4>
+                        <p class="card-text">Latest reviewed</p>
+                        <div class="list-group list-group-flush">
+                            <a href="#" class="list-group-item">Cras justo odio</a>
+                            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                            <a href="#" class="list-group-item">Morbi leo risus</a>
+                            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                            <a href="#" class="list-group-item">Vestibulum at eros</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-        </div>
-
             <div class="card">
-                <img class="card-img-top" src="../images/platforms.jpg" alt="games image">
+                <img class="card-img-top" src="../images/platforms.jpg" alt="platforms image">
                 <div class="card-body">
                     <h4 class="card-title">Platforms</h4>
                     <p class="card-text">Latest added</p>
@@ -41,9 +36,8 @@
                     </div>
                 </div>
             </div>
-
             <div class="card">
-                <img class="card-img-top" src="../images/categories.png" alt="games image">
+                <img class="card-img-top" src="../images/categories.png" alt="categories image">
                 <div class="card-body">
                     <h4 class="card-title">Categories</h4>
                     <p class="card-text">Latest added</p>
@@ -56,14 +50,10 @@
                     </div>
                 </div>
             </div>
-
         </div>
-
-
         <div class="row d-flex justify-content-around">
-
             <div class="card">
-                <img class="card-img-top" src="../images/users.jpeg" alt="games image">
+                <img class="card-img-top" src="../images/users.jpeg" alt="users image">
                 <div class="card-body">
                     <h4 class="card-title">Users</h4>
                     <p class="card-text">Latest added</p>
@@ -76,26 +66,5 @@
                     </div>
                 </div>
             </div>
-
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
-
-
-
 @endsection
