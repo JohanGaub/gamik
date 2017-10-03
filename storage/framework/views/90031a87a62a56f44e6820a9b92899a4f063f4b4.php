50cafbb054767e5bc7b7f5ff8518309d138bd0ce
JohanGaub <johangaub@gmail.com>
2017-10-03 10:17:51 +0200

    popper.js resolved in admin-template

--- storage/framework/views/90031a87a62a56f44e6820a9b92899a4f063f4b4.php
+++ storage/framework/views/90031a87a62a56f44e6820a9b92899a4f063f4b4.php
@@ -16,5 +16,6 @@
         src="https://code.jquery.com/jquery-3.2.1.js"
         integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
         crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/popper.min.js"></script>
+
+<script src="https://getbootstrap.com/assets/js/vendor/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
